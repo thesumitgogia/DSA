1356ece0c7facb8f2c9b42d70161311aef7e591d
Sumit Gogia <sumitgogia.developer@gmail.com>
2023-12-24 21:56:37 +0530

    project updated

--- Data Structure/LinkedList/Circular_Doubly_Linked_List.py
+++ Data Structure/LinkedList/Circular_Doubly_Linked_List.py
@@ -1,60 +1,57 @@
 class Node:
-    def __init__(self, prev=None, data=None, next=None):
-        self.prev = prev
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
 
 
-class CircularDoublyLinkedList:
+class CircularLinkedList:
     def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
 
     def insertAtBeginning(self, value):
-        node = Node(None, value)
+        node = Node(value)
         if not self.head:
             self.head = node
-            self.tail = node
             node.next = node
-            node.prev = node
-        self.head.prev = node
+            self.tail = node
         node.next = self.head
-        node.prev = self.tail
         self.head = node
-        self.head.prev = self.tail
-        self.tail.next = node
+        self.tail.next = self.head
 
     def insertAtTail(self, value):
-        node = Node(None, value)
-        if not self.head:
-            self.head = node
+        node = Node(value)
+        if not self.tail:
             self.tail = node
-            node.next = node
-            node.prev = node
-
-        self.tail.next = node
-        self.head.prev = node
-        node.prev = self.tail
+            node.next = self.tail
+            self.head = node
         node.next = self.head
+        self.tail.next = node
         self.tail = node
 
     def insertAfterNode(self, node, value):
         if not self.head:
-            print('List is empty')
+            print("List is empty")
             return
-        if self.head.data == node:
-            self.insertAtBeginning(value)
         if self.tail.data == node:
             self.insertAtTail(value)
+        if self.head.data == node:
+            self.insertAtBeginning(value)
         temp = self.head
-        new_node = Node(None, value)
+        new_node = Node(value)
         while temp != self.tail:
-            if temp.data == node:
+            if node == temp.data:
                 new_node.next = temp.next
-                temp.next.prev = new_node
                 temp.next = new_node
-                new_node.prev = temp
-
+                return
             temp = temp.next
 
 
+cll = CircularLinkedList()
+cll.insertAtBeginning(40)
+cll.insertAtBeginning(30)
+cll.insertAtBeginning(20)
+cll.insertAtBeginning(50)
+cll.insertAtTail(80)
+cll.insertAfterNode(30, 90)
+print(cll.head.next.next.next.next.next.next.data)
--- Data Structure/LinkedList/sample.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
-
-
-node = Node(20, None)
-node2 = Node(30, node)
-print(node2.data)
