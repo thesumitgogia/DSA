3a9b74f8a5e53cdb00bbe9da1496498c604f25c2
Sumit Gogia <sumitgogia.developer@gmail.com>
2023-12-25 18:06:01 +0530

    circular doubly linked list

--- Data Structure/LinkedList/Circular_Doubly_Linked_List.py
+++ Data Structure/LinkedList/Circular_Doubly_Linked_List.py
@@ -1,10 +1,11 @@
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, prev=None, next=None):
         self.data = data
+        self.prev = prev
         self.next = next
 
 
-class CircularLinkedList:
+class CircularDoublyLinkedList:
     def __init__(self, head=None, tail=None):
         self.head = head
         self.tail = tail
@@ -15,19 +16,23 @@ class CircularLinkedList:
             self.head = node
             node.next = node
             self.tail = node
+            node.prev = node
+        self.head.prev = node
         node.next = self.head
+        node.prev = self.tail
         self.head = node
         self.tail.next = self.head
 
     def insertAtTail(self, value):
-        node = Node(value)
         if not self.tail:
-            self.tail = node
-            node.next = self.tail
-            self.head = node
-        node.next = self.head
+            self.insertAtBeginning(value)
+            return
+        node = Node(value)
+        node.next = self.tail.next
         self.tail.next = node
+        node.prev = self.tail
         self.tail = node
+        self.head.prev = node
 
     def insertAfterNode(self, node, value):
         if not self.head:
@@ -35,23 +40,47 @@ class CircularLinkedList:
             return
         if self.tail.data == node:
             self.insertAtTail(value)
-        if self.head.data == node:
-            self.insertAtBeginning(value)
+            return
         temp = self.head
         new_node = Node(value)
         while temp != self.tail:
             if node == temp.data:
                 new_node.next = temp.next
+                new_node.prev = temp
+                temp.next.prev = new_node
                 temp.next = new_node
                 return
             temp = temp.next
 
+    def __iter__(self):
+        current = self.head
+        while current != self.tail:
+            yield current.data
+            current = current.next
+        yield current.data
 
-cll = CircularLinkedList()
-cll.insertAtBeginning(40)
+
+cll = CircularDoublyLinkedList()
+# cll.insertAtBeginning(40)
+# cll.insertAtBeginning(30)
+# cll.insertAtBeginning(20)
+# cll.insertAtBeginning(50)
+# cll.insertAtTail(80)
+# cll.insertAfterNode(30, 90)
+# print(cll.head.next.next.next.next.next.next.data)
 cll.insertAtBeginning(30)
-cll.insertAtBeginning(20)
+cll.insertAtBeginning(40)
 cll.insertAtBeginning(50)
 cll.insertAtTail(80)
-cll.insertAfterNode(30, 90)
-print(cll.head.next.next.next.next.next.next.data)
+cll.insertAtTail(60)
+# print(cll.tail.data)
+cll.insertAfterNode(60, 90)
+# print(cll.tail.data)
+# print(cll.head.next.next.next.data)
+cll.insertAfterNode(30, 120)
+# print(cll.head.next.next.next.next.next.next.data)
+# print(cll.head.next.data)
+cll.insertAfterNode(50, 140)
+# print(cll.head.next.data)
+for element in cll:
+    print(element)
